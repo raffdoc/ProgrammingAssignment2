1bf48cf0046c33b8941639d9bca0d1600afed387
raffdoc <raffdoc@yahoo.com>
2014-04-26 22:50:31 +0200

    Two functions seem to work well.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 # This is new!.
 makeCacheMatrix <- function(x = matrix()) {
+        CacheMatrix <- NULL
+        set <- function(mtrx) {
+                x <<- mtrx
+                CacheMatrix <<- NULL
+        }
+        get <- function() x
+        setMatrix <- function(parmMatrix) CacheMatrix <<- parmMatrix
+        getMatrix <- function() CacheMatrix
+        list(set = set, get = get,
+             setMatrix = setMatrix,
+             getMatrix = getMatrix)
+        
 
 }
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        CacheMatrix <- x$getMatrix()
+        if(!is.null(CacheMatrix)) {
+                message("getting inverse matrix")
+                return(CacheMatrix)
+        }
+        data <- x$get()
+        CacheMatrix <- solve(data, ...)
+        x$setMatrix(CacheMatrix)
+        CacheMatrix
 }
